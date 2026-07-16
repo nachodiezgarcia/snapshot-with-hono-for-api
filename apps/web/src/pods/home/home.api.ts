0078b1461adaa81e9656b85366dfce9f549169ad
nachodiezgarcia <diezgarcianacho@gmail.com>
2026-07-16 12:27:42 +0200

    00-two-minor-changes

--- apps/web/src/pods/home/home.api.ts
+++ apps/web/src/pods/home/home.api.ts
@@ -3,7 +3,7 @@ import type { HomeSection } from './home.model';
 import { mapHomeSectiontoVm } from "./home.mapper";
 
 export const getHomePageContent = createServerFn({ method: 'GET' }).handler(async () => {
-  const apiUrl = process.env.API_URL ?? 'http://localhost:3001';
+  const apiUrl = process.env.PUBLIC_API_BASE_URL ?? 'http://localhost:3001';
   const response = await fetch(`${apiUrl}/api/home`);
   const content: HomeSection = await response.json();
   return mapHomeSectiontoVm(content);
--- apps/web/src/pods/home/home.pod.tsx
+++ apps/web/src/pods/home/home.pod.tsx
@@ -1,10 +1,11 @@
+import type { FC } from "react";
 import type { HomeSectionVm } from "./home.vm";
 
 interface HomeProps {
   content: HomeSectionVm;
 }
 
-export const Home: React.FC<HomeProps> = ({ content }) => {
+export const Home: FC<HomeProps> = ({ content }) => {
   return (
     <div>
       <h1>{content.mainText}</h1>
