040a8f8ec4ff9cf0c018e385e1859569229682da
nachodiezgarcia <diezgarcianacho@gmail.com>
2026-07-17 12:02:48 +0200

    prueba_con_contenido_harcodeado

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,10 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Home, getHomePageContent } from '@/pods/home/home.index';
+//import { Home, getHomePageContent } from '@/pods/home/home.index';
 
 
 export const Route = createFileRoute('/')({
   loader: async() =>  {
-    const content = await getHomePageContent();
+    const content = 'Mensaje';//await getHomePageContent(); Y en el return( <Home content={content} />)
     return { content };
   },
   component: HomePage
@@ -12,7 +12,5 @@ export const Route = createFileRoute('/')({
 
 function HomePage() {
   const { content } = Route.useLoaderData();
-  return (
-    <Home content={content} />
-  )
+  return <h1>{content}</h1>
 }
